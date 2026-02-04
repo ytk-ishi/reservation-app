f468100f743deb5b78dc4f90cbec9943b12fba62
ytk-ishi <y.ishikawa.for.work0651@gmail.com>
2026-02-04 16:31:57 +0900

    ヘッダーコンポーネント追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavberComponent } from '../app/common/navbar/navber.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavberComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/common/navbar/navber.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navber',
+  templateUrl: './navber.component.html',   
+  styleUrls: ['./navber.component.scss',],
+  standalone: true,
+})
+
+export class NavberComponent {
+}
