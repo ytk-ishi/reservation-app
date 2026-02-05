184ed55abc412d03371bb147e248d189cf73d983
ytk-ishi <y.ishikawa.for.work0651@gmail.com>
2026-02-05 20:06:20 +0900

    Nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,76 @@
+const Product = require('./model/product')
+
+class FakeDB {
+
+    constructor() {
+        this.products = [
+            {
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                coverImage: './assets/img/phone-cover.jpg',
+                heading1: 'サンプルテキスト1',
+                heading2: 'サンプルテキスト2',
+                heading3: 'サンプルテキスト3',
+                headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+                headingText2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+                headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+            },
+            {
+              name: 'Phone Mini',
+              price: 699,
+              description: 'A great phone with one of the best cameras',
+              coverImage: './assets/img/phone-cover.jpg',
+              heading1: 'サンプルテキスト1',
+              heading2: 'サンプルテキスト2',
+              heading3: 'サンプルテキスト3',
+              headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+              headingText2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+              headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+            },
+            {
+              name: 'Phone Standard',
+              price: 299,
+              description: '',
+              coverImage: './assets/img/phone-cover.jpg',
+              heading1: 'サンプルテキスト1',
+              heading2: 'サンプルテキスト2',
+              heading3: 'サンプルテキスト3',
+              headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+              headingText2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+              headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+            },
+            {
+              name: 'Phone Special',
+              price: 999,
+              description: '',
+              coverImage: './assets/img/phone-cover.jpg',
+              heading1: 'サンプルテキスト1',
+              heading2: 'サンプルテキスト2',
+              heading3: 'サンプルテキスト3',
+              headingText1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+              headingText2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+              headingText3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+            }
+        ]
+    }
+
+
+    pushProductsToDb() {
+        this.products.forEach(product => {
+            const newProduct = new Product(product);
+            newProduct.save();
+        });
+    }
+
+    async initDb() {
+        await this.cleanDb();
+        this.pushProductsToDb();
+    }
+
+    async cleanDb() {
+        await Product.deleteMany({});
+    }
+}
+
+module.exports = FakeDB;
