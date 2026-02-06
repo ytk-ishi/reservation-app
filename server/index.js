d19e6303f797d55e0dcb8d40a65cadb1be6978b6
ytk-ishi <y.ishikawa.for.work0651@gmail.com>
2026-02-06 17:26:40 +0900

    Deploy用セットアップ

--- server/index.js
+++ server/index.js
@@ -1,22 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev');
+const config = require('./config');
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.connect(config.DB_URI).then(
     () => {
-        const fakeDb = new FakeDb();
-        fakeDb.initDb();
+        if (process.env.NODE_ENV !== 'production') {
+            const fakeDb = new FakeDb();
+            // テスト環境で、DB初期化したい時だけコメント解除
+            // fakeDb.initDb();
+        }
     }
 );
 
 const app = express();
-const PORT = process.env.PORT || '3001';
 
 app.use('/api/v1/products', productRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    const appPath = path.join(__dirname, '..', 'dist', 'reservation-app', 'browser');
+    app.use(express.static(appPath));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(appPath, 'index.html'));
+    });
+}
+
+const PORT = process.env.PORT || '3001';
 app.listen(PORT, function() {
     console.log('I am running!');
 })
